7b0b811d0a90230ded96f95432035260e6f7af60
lesshst <lesshst.lee@pku.edu.cn>
2016-03-11 15:49:28 +0800

    ignore some files

--- FRManager/FRManager_i.c
+++ FRManager/FRManager_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Mar 11 14:34:50 2016
+/* at Fri Mar 11 15:46:01 2016
  */
 /* Compiler settings for FRManager.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
